a786ff383c4b4183b48b0bf58b33c96099124bbe
msky8778878 <michalskymi@gmail.com>
2024-05-06 15:29:04 +0200

    Completed Learning03 activity

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,33 @@
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_top.ToString()}/{_bottom.ToString()}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction first = new Fraction();
+        Fraction second = new Fraction(6);
+        Fraction third = new Fraction(6, 7);
+
+        Console.WriteLine($"{third.GetFractionString()} = {third.GetDecimalValue()}");
     }
 }
